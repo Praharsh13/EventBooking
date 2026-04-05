fc966b922364f49e7f983b8cf3865a042a3db0eb
Praharsh Pranjal <ppjha.13@gmail.com>
2026-04-05 01:13:16 +0100

    Added comman share code

--- apps/api-gateway/src/app.service.ts
+++ apps/api-gateway/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { Port, Service } from '@app/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `${Service.API_GATEWAY} is running on Port ${Port.API_GATEWAY}`;
   }
 }
---
+++ libs/common/src/constants/constant.ts
@@ -0,0 +1,18 @@
+export const Service={
+
+    API_GATEWAY:"api-gateway",
+    AUTH_SERVICE:"auth-service",
+    USER_SERVICE:"user-service"
+
+} 
+
+
+export const Port={
+
+    API_GATEWAY:3000,
+    AUTH_SERVICE:3001,
+    USER_SERVICE:3002
+
+}
+
+
