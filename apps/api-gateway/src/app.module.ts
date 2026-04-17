f6ce5fa853c9f665fa69b6d6a6eebf5f5b5e181a
Praharsh Pranjal <ppjha.13@gmail.com>
2026-04-17 21:13:21 +0100

    Feat:Event service - api gateway created

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { EventsService } from './events/events.service';
+import { EventsController } from './events/events.controller';
+import { EventsModule } from './events/events.module';
+import { JwtStrategy } from './jwt.startegy';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, EventsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, JwtStrategy],
 })
 export class AppModule {}
